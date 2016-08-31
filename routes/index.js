a9b6ab5439e1226bfacce7c70389ecda1b5e5fa3
Julian Haeger <julianhaeger@gmail.com>
2016-08-31 17:48:20 +0100

    can accept string or int arguments from url query while executing

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,24 @@ var router = express.Router();
 var scripts = require('../scripts');
 const vm = require('vm');
 
+function contextFromQuery(query){
+  return Object.keys(query).reduce((context, key) => {
+    let value = query[key];
+    if(/-?\d+/.test(value)){
+      value = parseInt(value);
+    }
+    context[key] = value;
+    return context;
+  }, {});
+}
 
 /* GET home page. */
 router.get('/execute/:scriptId', function(req, res, next) {
   const scriptId = req.params.scriptId;
   scripts.get(scriptId).then(script => {
-    const result = vm.runInNewContext(script, {});
+    const context = contextFromQuery(req.query);
+    console.log('context', context);
+    const result = vm.runInNewContext(script, context);
     res.json({scriptId:scriptId, script:script, result:result});
   });
 });
